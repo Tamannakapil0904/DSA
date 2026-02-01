fbd90a641376bd98a1342ca7414206cb498f65ed
T.K <119012737+Tamannakapil0904@users.noreply.github.com>
2026-02-02 02:48:03 +0530

    Create Divide_an_ArrayIntoSubarraysWithMinimumCost_I.java

---
+++ Divide_an_ArrayIntoSubarraysWithMinimumCost_I.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minimumCost(int[] nums) {
+        int n = nums.length;
+        int minCost = Integer.MAX_VALUE;
+        for(int i = 1; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                minCost = Math.min(minCost, nums[0] + nums[i] + nums[j]);
+            }
+        }
+        return minCost;
+    }
+}
